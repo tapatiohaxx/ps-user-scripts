fb93f1b76093355fcf709cd51fc61c1a162766c3
tapatiohaxx <ilovemyramen@gmail.com>
2022-07-03 13:39:52 -0700

    Changes the theme to 'festivetech' and  correct date

--- user_profile.ps1
+++ user_profile.ps1
@@ -1,6 +1,10 @@
+#Function to generate date for powershell festivetech theme
+function Set-EnvVars {
+$now = [DateTime]::Now
+$env:DAYS = ([Datetime](“12/25/” + $now.Year) – $Now).Days
+}
+New-Alias -Name 'Set-PoshContext' -Value 'Set-EnvVars' -Scope Global -Force
 
-set-PoshPrompt Paadox
-
-
-oh-my-posh.exe init pwsh | Invoke-Expression
+#change to whatever theme you like in the [--config] section
+oh-my-posh.exe init pwsh --config D:\themes\festivetech.omp.json | Invoke-Expression
 
