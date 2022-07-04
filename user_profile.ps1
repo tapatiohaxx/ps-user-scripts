246eb6e4d22d3d1e449de71ebb78fab56d9ca4a5
tapatiohaxx <iovemyramen@gmail.com>
2022-07-03 17:25:04 -0700

    Adds Aliases

--- user_profile.ps1
+++ user_profile.ps1
@@ -8,3 +8,9 @@ New-Alias -Name 'Set-PoshContext' -Value 'Set-EnvVars' -Scope Global -Force
 #change to whatever theme you like in the [--config] section
 oh-my-posh.exe init pwsh --config D:\themes\festivetech.omp.json | Invoke-Expression
 
+#Aliases
+New-Alias -Name .. -Value cd..
+New-Alias -Name vim -Value 'C:\Program Files\Vim\vim82/vim.exe'
+New-Alias -name code -value 'C:\Users\ilove\AppData\Local\Programs\Microsoft VS Code Insiders\Code - Insiders.exe'
+New-Alias -Name firefox -Value 'C:\Program Files\Mozilla Firefox\firefox.exe'
+
