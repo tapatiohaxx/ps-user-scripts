595fb0061d0fb5745181178e2ff311ceb7fe8112
tapatiohaxx <ilovemyramen@gmail.com>
2022-07-04 14:25:14 -0700

    removes program-specific aliases from ser_profile.ps1

--- user_profile.ps1
+++ user_profile.ps1
@@ -8,11 +8,6 @@ New-Alias -Name 'Set-PoshContext' -Value 'Set-EnvVars' -Scope Global -Force
 #change to whatever theme you like in the [--config] section
 oh-my-posh.exe init pwsh --config D:\themes\festivetech.omp.json | Invoke-Expression
 
-<<<<<<< HEAD
 #You may use these aliases tto accomplish functions that I do on a regular basis. Certain downloadable programs you may have may make using these aliases unnecessary.
-
 new-alias -name .. -value cd..
-`New-Alias -Name vim -Value 'C:\Program Files\Vim\vim82/vim.exe'
-New-Alias -name code -value 'C:\Users\ilove\AppData\Local\Programs\Microsoft VS Code Insiders\Code - Insiders.exe'
-New-Alias -Name firefox -Value 'C:\Program Files\Mozilla Firefox\firefox.exe'
 
